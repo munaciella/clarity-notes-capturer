1171846ea987ab72a0a1994a8b0a6503eb385c05
munaciella <franes88@gmail.com>
2025-01-07 15:17:26 +0000

    initial commit and set up shadcn-ui

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main >
+      <h1>Let's build an AI productivity app</h1>
+
+      <Button>Click me</Button>
+    </main>
+  );
+}
