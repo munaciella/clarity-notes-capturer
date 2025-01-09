db4008b86f60badce640bc795e747245cec0db33
munaciella <franes88@gmail.com>
2025-01-09 14:46:14 +0000

    add header to app with clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
+import Header from '@/components/Header';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,7 +16,15 @@ export default function RootLayout({
   return (
       <ClerkProvider>
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header />
+
+        <div className="flex min-h-screen">
+          {/* Sidebar */}
+
+        <div className='flex-1 p-4 bg-gray-100 overflow-y-auto scrollbar-hide'>{children}</div>
+        </div>
+        </body>
     </html>
     </ClerkProvider>
   );
