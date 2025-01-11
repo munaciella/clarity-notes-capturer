d7b49892cdc619fb5da287206444bde149d9bb64
munaciella <franes88@gmail.com>
2025-01-11 14:24:27 +0000

    create new sidebar option component & add new docs option to sidebar when creating new document

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,9 @@
+'use client';
+
 import { MenuIcon } from 'lucide-react';
 import NewDocumentButton from './NewDocumentButton';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { useUser } from '@clerk/nextjs';
 
 import {
   Sheet,
@@ -8,14 +12,94 @@ import {
   SheetTitle,
   SheetTrigger,
 } from '@/components/ui/sheet';
+import { collectionGroup, DocumentData, query, where } from 'firebase/firestore';
+import { db } from '../../firebase';
+import { useEffect, useState } from 'react';
+import SidebarOption from './SidebarOption';
+
+interface RoomDocument extends DocumentData {
+    createdAt: string
+    role: "owner" | "editor";
+    roomId: string;
+    userId: string;
+}
 
 const Sidebar = () => {
+  const { user, isSignedIn } = useUser();
+  const [groupedData, setGroupedData] = useState<{
+    owner: RoomDocument[];
+    editor: RoomDocument[];
+  }>({
+    owner: [],
+    editor: [],
+  });
+
+  const userEmail = isSignedIn && user?.emailAddresses?.[0]?.emailAddress;
+
+  const [data, loading, error] = useCollection(
+    userEmail
+      ? query(
+          collectionGroup(db, 'rooms'),
+          where('userId', '==', userEmail)
+        )
+      : null
+  );
+
+  useEffect(() => {
+    if(!data) return;
+
+    const grouped = data.docs.reduce<{
+        owner: RoomDocument[];
+        editor: RoomDocument[];
+    }>(
+        (acc, curr) => {
+            const roomData = curr.data() as RoomDocument;
+            if (roomData.role === 'owner') {
+                acc.owner.push({
+                    id: curr.id,
+                    ...roomData,
+                });
+            } else {
+                acc.editor.push({
+                    id: curr.id,
+                    ...roomData,
+                });
+            }
+            return acc;
+        }, {
+            owner: [],
+            editor: [],
+        }
+    )
+    setGroupedData(grouped);
+  }, [data]);
+
+  if (loading) {
+    return <p className="p-2">Loading...</p>;
+  }
+
+  if (error) {
+    return <p className="p-2 text-red-500">Error loading documents</p>;
+  }
+
   const menuOptions = (
     <>
       <NewDocumentButton />
 
+      <div className='flex py-4 flex-col space-y-4 md:max-w-36'>
       {/* My documents */}
-      {/* List... */}
+      {groupedData.owner.length === 0 ? (
+        <h2 className="text-gray-500 font-semibold text-sm">No documents found</h2>
+      ): (
+        <>
+        <h2 className="text-gray-500 font-semibold text-sm">My documents</h2>
+        {groupedData.owner.map((doc) => (
+            <SidebarOption key={doc.id} id={doc.id} href={`/doc/${doc.id}`} />
+        ))}
+        </>
+      )}
+      </div>
+
 
       {/* Shared with me */}
       {/* List... */}
