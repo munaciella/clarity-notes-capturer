e5a898e3b8ac6234c3f5d138f01da36bca1cc1b1
munaciella <franes88@gmail.com>
2025-01-27 11:45:21 +0000

    create a sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     siteUrl: 'https://www.clarity-capture.com',
     generateRobotsTxt: true, // (Optional) Generate robots.txt
+    exclude: ['/404', '/500'], // (Optional) Sitemap exclude
   };
